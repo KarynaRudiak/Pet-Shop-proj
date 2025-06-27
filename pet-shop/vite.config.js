65f2f9374e558662925d2c4a8e836c3c92cc5a73
KarynaRudiak <karinaarudiak@gmail.com>
2025-06-27 15:05:56 +0200

    Fix homepage URL for GitHub Pages

--- pet-shop/vite.config.js
+++ pet-shop/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Pet-Shop2.0/',
+  base: '/Pet-Shop-proj/',
   plugins: [react()],
   build: {
     outDir: 'dist',
