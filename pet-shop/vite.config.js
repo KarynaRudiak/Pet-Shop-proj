99cfbb0a998987665e9baad96bf1f12ba545960c
KarynaRudiak <karinaarudiak@gmail.com>
2025-06-27 15:47:03 +0200

    add netlify.toml

--- pet-shop/vite.config.js
+++ pet-shop/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Pet-Shop-proj/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
